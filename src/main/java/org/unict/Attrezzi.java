d06400f385c756888c7e12f4ae3722644def813c
terranovamtt <terranovamtt@gmail.com>
2022-02-25 14:19:08 +0100

    MTTcommit

--- src/main/java/org/unict/Attrezzi.java
+++ src/main/java/org/unict/Attrezzi.java
@@ -1,4 +1,22 @@
 package org.unict;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Attrezzi {
+    private String idAttrezzo;
+
+    public Attrezzi(String idAttrezzo){
+        this.idAttrezzo = idAttrezzo;
+    }
+
+    public String getIdAttrezzo() {
+        return idAttrezzo;
+    }
+
+    public void setIdAttrezzo(String idAttrezzo) {
+        this.idAttrezzo = idAttrezzo;
+    }
 }
--- src/main/java/org/unict/Corso.java
+++ src/main/java/org/unict/Corso.java
@@ -1,4 +1,52 @@
 package org.unict;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Corso {
+    private String idCorso;
+    private String  nome;
+    private String  livello;
+    private String  focus;
+
+    public Corso(String idCorso, String nome, String livello, String focus){
+        this.idCorso =idCorso;
+        this.nome=nome;
+        this.livello=livello;
+        this.focus=focus;
+    }
+
+    public String getIdCorso() {
+        return idCorso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLivello() {
+        return livello;
+    }
+
+    public String getFocus() {
+        return focus;
+    }
+
+    public void setIdCorso(String idCorso) {
+        this.idCorso = idCorso;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setLivello(String livello) {
+        this.livello = livello;
+    }
+
+    public void setFocus(String focus) {
+        this.focus = focus;
+    }
+
+
 }
