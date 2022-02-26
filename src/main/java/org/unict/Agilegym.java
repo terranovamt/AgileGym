0fadd0001f895745a8e65b5cf343d6259b3c42c7
Fabiola Marchì <mrcfbl99s48c342j@studium.unict.it>
2022-02-26 09:52:59 +0100

    FabiolaCommit

--- src/main/java/org/unict/Agilegym.java
+++ src/main/java/org/unict/Agilegym.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class Agilegym {
     //attributes
+    private int n= 5;
     private static  Agilegym agilegym;
     private Corso corsoCorrente;
     private String idAttrezzo;
@@ -20,6 +21,10 @@ public class Agilegym {
         this.elencoIstruttoriDisponibili = new HashMap<>();
         this.elencoLezioni = new HashMap<>();
         this.elencoSaleDisponibili = new HashMap<>();
+        loadSale();
+        //loadIstruttore();
+        //loadAttrezzi();
+
 
     }
 
@@ -42,16 +47,45 @@ public class Agilegym {
         Sala s = elencoSaleDisponibili.get(idAttrezzo);
         Istruttore i = elencoIstruttoriDisponibili.get(idSlot);
             if(s != null){
-                this.corsoCorrente.inserisciLezione(idLezione, idSlot ,s, i);
+                this.corsoCorrente.inserisciLezione(idLezione, idSlot, s, i);
                 System.out.println("Lezione Inserita");
             }
             else
                 System.out.println("Sala occupata");
 
-        System.out.println("Corso inserito");
+
 
     }
+    public  void conferma(){
+        if(corsoCorrente != null){
+            this.elencoCorsi.put(corsoCorrente.getIdCorso(), corsoCorrente);
+            System.out.println("Il corso e'stato inserito!");
+        }
+    }
 
+    public void loadSale(){
+        int i;
+        Sala[] s = new Sala[n];
+        for(i=0; i<=n; i++){
+
+            s[i] = new Sala(String.valueOf(i));
+            this.elencoSaleDisponibili.put(String.valueOf(i), s[i]);
+
+        }
+        System.out.println("Elenco Sale caricato!");
+    }
+    /*public void loadIstruttore(){
+        int j;
+        Istruttore[] i = new Istruttore[n];
+        for(j=0; j<=n; j++){
+
+            i[j] = new Istruttore(String.valueOf(j));
+            this.elencoSaleDisponibili.put(String.valueOf(i), s[i]);
+
+        }
+        System.out.println("Elenco Sale caricato!");
+
+    }*/
 
 
 
--- src/main/java/org/unict/Istruttore.java
+++ src/main/java/org/unict/Istruttore.java
@@ -3,9 +3,11 @@ package org.unict;
 public class Istruttore {
 
     private String idIstruttore;
+    private int idSlot;     //primo numero giorno della settimana e i successivi due indicano l'ora. N.B. 209= martedi' ore 9 am
 
-    public Istruttore(String idIstruttore){
+    public Istruttore(String idIstruttore, int idSlot){
          this.idIstruttore = idIstruttore;
+         this.idSlot = idSlot;
      }
 
     public String getIdIstruttore() {
@@ -14,5 +16,13 @@ public class Istruttore {
 
     public void setIdIstruttore(String idIstruttore) {
         this.idIstruttore = idIstruttore;
+    }
+
+    public int getIdSlot() {
+        return idSlot;
+    }
+
+    public void setIdSlot(int idSlot) {
+        this.idSlot = idSlot;
     }
 }
