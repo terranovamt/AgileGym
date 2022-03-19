063929b09712ed8c29115de065eecc723e916d66
FedericaRizza <uni303002@studium.unict.it>
2022-03-19 16:31:18 +0100

    Aggiunte classi di test CorsoTest.java e IstruttoreTest.java

--- src/test/java/org/unict/CorsoTest.java
+++ src/test/java/org/unict/CorsoTest.java
@@ -2,18 +2,19 @@ package org.unict;
 
 import org.junit.*;
 import org.unict.domain.*;
+import org.unict.domain.exception.CorsoException;
 
 import java.util.*;
 
 
 public class CorsoTest {
-    //nome_scenario_expected add_singlenumber_returnsamenumber
+
     Corso c;
     Attrezzo a;
-    List<String> l;
+    List<String> l,n;
     Istruttore i;
     Sala s;
-    Slot o;
+    String o;
 
 
     @Before
@@ -23,8 +24,11 @@ public class CorsoTest {
         l.add("Sala2");
         a= new Attrezzo("tappetino",l);
         c= new Corso("1000","Yoga","principiante","equilibrio",a);
-        o=new Slot("216", true);
+        o="216";
         s= new Sala("Sala1");
+        n=  new LinkedList<>();
+        n.add("tappetino");
+        s.setListaAttrezzi(n);
         i= new Istruttore("Fabiola");
     }
 
@@ -35,11 +39,40 @@ public class CorsoTest {
         o=null;
         s=null;
         i=null;
+        l.clear();
+        n.clear();
+
     }
     @Test
-    public void inserisciILezioneTest_datiValidi_returnTrue(){
+    public void inserisciILezioneTest_datiValidi_returnTrue() throws CorsoException {
         c.inserisciLezione("233445", o,c,s,i);
         Assert.assertTrue(c.getElencoLezioni().containsKey("233445"));
         Assert.assertEquals (1,c.getElencoLezioni().size());
+    }
+
+    @Test
+    public void inserisciILezioneTest_salaErrata_throwsException() {
+        s= new Sala("Sala5");
+        n=  new LinkedList<>();
+        n.add("tappetino");
+        s.setListaAttrezzi(n);
+        Throwable e = Assert.assertThrows(CorsoException.class, ()-> c.inserisciLezione("233445", o,c,s,i));
+        Assert.assertEquals(CorsoException.class,e.getClass());
+    }
+
+    @Test
+    public void inserisciILezioneTest_slotSalaOccupato_throwsException() throws CorsoException{
+        c.inserisciLezione("233445", o,c,s,i);
+        s.setOccupato(o);
+        Throwable e = Assert.assertThrows(CorsoException.class, ()-> c.inserisciLezione("233446", o,c,s,i));
+        Assert.assertEquals(CorsoException.class,e.getClass());
+    }
+
+    @Test
+    public void inserisciILezioneTest_slotIstruttoreOccupato_throwsException() throws CorsoException{
+        c.inserisciLezione("233445", o,c,s,i);
+        i.setOccupato(o);
+        Throwable e = Assert.assertThrows(CorsoException.class, ()-> c.inserisciLezione("233446", o,c,s,i));
+        Assert.assertEquals(CorsoException.class,e.getClass());
     }
 }
